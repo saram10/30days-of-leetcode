ddd2021abd19626ea5f32c66babd5c5dc2d8dcec
saram10 <117080812+saram10@users.noreply.github.com>
2024-03-26 06:47:19 +0500

    Time: 8 ms (71.89%), Space: 16.1 MB (47.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,18 +3,18 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int p=matrix[0].size();
         int q=matrix.size();
-        bool FirstRowHasZero=false;
-        bool FirstColumnHasZero=false;
+        bool FR=false;
+        bool FC=false;
         
         for(int i=0;i<p;i++){
             if(matrix[0][i]==0) {
-                FirstRowHasZero=true;
+                FR=true;
                 break;
             }
         }
         for(int i=0;i<q;i++){
             if(matrix[i][0]==0){
-                FirstColumnHasZero=true;
+                FC=true;
                 break;
             }
         }
@@ -33,19 +33,19 @@ public:
                 }
             }
         }
-        for(int j=1;j<p;j++){
-            if(matrix[0][j]==0){
-                for(int i=1;i<q;i++){
-                    matrix[i][j]=0;
+        for(int i=1;i<p;i++){
+            if(matrix[0][i]==0){
+                for(int j=1;j<q;j++){
+                    matrix[j][i]=0;
                 }
             }
         }
-        if(FirstRowHasZero){
+        if(FR){
             for(int i=0;i<p;i++){
                 matrix[0][i]=0;
             }
         }
-        if(FirstColumnHasZero){
+        if(FC){
             for(int i=0;i<q;i++){
                 matrix[i][0]=0;
             }
